8bee1e722850d2ba3f294d20eaa574187028c44b
YangDong <yod1073@gmail.com>
2023-01-18 01:15:42 +0900

    make `utils.py`

--- utils.py
+++ utils.py
@@ -1,6 +1,23 @@
 import datetime as dt
 
-def get_date(date1: str, date2: str=None):
+def get_date(date1: int, date2: int=None, /):
+    """
+    Return list of date(s)
+    
+    :param `date1`: first date
+    :param `date2`: (Optional) second date
+    
+    :return:
+        date list from `date1` to `date2`
+    """
     if not date2:
         return [date1]
     
+    date:int = date1
+    date_list = list()
+    while date <= date2:
+        date_list.append(date)
+        date = dt.datetime.strptime(str(date), "%Y%m%d") + dt.timedelta(days=1)
+        date = int(date.strftime('%Y%m%d'))
+    
+    return date_list
